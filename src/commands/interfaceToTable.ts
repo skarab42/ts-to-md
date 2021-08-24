981dfd4cdced0250453706af8a37aaca41de3b9e
skarab42 <skarab@bluewin.ch>
2021-08-24 15:19:09 +0200

    get some basic info: name, docs, members

--- src/commands/interfaceToTable.ts
+++ src/commands/interfaceToTable.ts
@@ -1,42 +1,68 @@
+import ts from "typescript";
+
 import * as vscode from "vscode";
-import * as ts from "typescript";
 import * as utils from "tsutils";
 
-export function interfaceToTable(this: vscode.ExtensionContext) {
+export async function interfaceToTable(this: vscode.ExtensionContext) {
   const editor = vscode.window.activeTextEditor;
 
   if (!editor) {
     return;
   }
 
-  const { document, selection } = editor;
-  const { line, character } = selection.start;
-  const { fileName } = document;
+  try {
+    const { document, selection } = editor;
+    const { fileName } = document;
+    const { line, character } = selection.start;
+    const program = ts.createProgram([fileName], {});
+    const sourceFile = program.getSourceFile(fileName);
 
-  const text = document.getText();
-  const source = ts.createSourceFile(fileName, text, ts.ScriptTarget.Latest);
-  const position = ts.getPositionOfLineAndCharacter(source, line, character);
-  const token = utils.getTokenAtPosition(source, position);
-
-  if (!token) {
-    return;
-  }
-
-  const { parent } = token;
-
-  if (parent && utils.isInterfaceDeclaration(parent)) {
-    const { name, members, heritageClauses } = parent;
-
-    console.log(`interface ${name.escapedText} {...}`, parent);
-
-    members.forEach((member) => {
-      console.log(">", member);
-    });
-
-    if (heritageClauses) {
-      heritageClauses.forEach((member) => {
-        console.log(">>", member);
-      });
+    if (!sourceFile) {
+      return;
     }
+
+    const position = ts.getPositionOfLineAndCharacter(
+      sourceFile,
+      line,
+      character
+    );
+
+    const token = utils.getTokenAtPosition(sourceFile, position);
+
+    if (!token) {
+      return;
+    }
+
+    const { parent } = token;
+
+    if (parent && utils.isInterfaceDeclaration(parent)) {
+      const checker = program.getTypeChecker();
+      const symbol = checker.getSymbolAtLocation(parent.name);
+
+      if (!symbol) {
+        return;
+      }
+
+      const members = symbol.members;
+      const name = symbol.escapedName;
+      const docs = ts.displayPartsToString(
+        symbol.getDocumentationComment(checker)
+      );
+
+      // console.log(">>>", { parent, token, symbol, members });
+      console.log(">>>", { name, docs });
+
+      if (members) {
+        members.forEach((member) => {
+          const type = checker.getTypeOfSymbolAtLocation(
+            member,
+            member.valueDeclaration!
+          );
+          console.log("---", member.escapedName, checker.typeToString(type));
+        });
+      }
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
