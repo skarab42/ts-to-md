d2d3df8aa4739d31859e2a2b6b1f12753f9b560b
skarab42 <skarab@bluewin.ch>
2021-08-24 10:53:15 +0200

    get interface token at selection start position

--- src/commands/interfaceToTable.ts
+++ src/commands/interfaceToTable.ts
@@ -1,5 +1,42 @@
 import * as vscode from "vscode";
+import * as ts from "typescript";
+import * as utils from "tsutils";
 
 export function interfaceToTable(this: vscode.ExtensionContext) {
-  console.log("Interface -> Table", this);
+  const editor = vscode.window.activeTextEditor;
+
+  if (!editor) {
+    return;
+  }
+
+  const { document, selection } = editor;
+  const { line, character } = selection.start;
+  const { fileName } = document;
+
+  const text = document.getText();
+  const source = ts.createSourceFile(fileName, text, ts.ScriptTarget.Latest);
+  const position = ts.getPositionOfLineAndCharacter(source, line, character);
+  const token = utils.getTokenAtPosition(source, position);
+
+  if (!token) {
+    return;
+  }
+
+  const { parent } = token;
+
+  if (parent && utils.isInterfaceDeclaration(parent)) {
+    const { name, members, heritageClauses } = parent;
+
+    console.log(`interface ${name.escapedText} {...}`, parent);
+
+    members.forEach((member) => {
+      console.log(">", member);
+    });
+
+    if (heritageClauses) {
+      heritageClauses.forEach((member) => {
+        console.log(">>", member);
+      });
+    }
+  }
 }
