a8cf0f084d427540a72dd2ae028abcdca8d1b8af
skarab42 <skarab@bluewin.ch>
2021-08-26 13:48:23 +0200

    import TS libraries

--- src/lib/ts-utils.ts
+++ src/lib/ts-utils.ts
@@ -1,4 +1,5 @@
 import path from "path";
+import glob from "glob";
 import { getTokenAtPosition } from "tsutils";
 import {
   Symbol,
@@ -15,6 +16,7 @@ import {
   isInterfaceDeclaration,
   getPositionOfLineAndCharacter,
 } from "typescript";
+import { readFileSync } from "fs";
 
 interface ProgramAndSourceFile {
   program: Program;
@@ -77,22 +79,51 @@ function createCompilerHost(virtualFS: VirtualFS): CompilerHost {
   };
 }
 
+const libFileNames: string[] = [];
+const libFiles: FSEntry[] = glob
+  .sync("**/node_modules/typescript/lib/lib.*.d.ts")
+  .map((fileName) => {
+    const shortFileName = path.basename(fileName);
+    libFileNames.push(shortFileName);
+    return {
+      fileName: shortFileName,
+      text: readFileSync(fileName, "utf-8"),
+    };
+  });
+
+console.log({ libFiles });
+
 export function createProgramAndGetSourceFile(
   fileName: string,
   text: string,
   options?: CompilerOptions
 ): ProgramAndSourceFile {
+  // const program = createProgram([fileName], options ?? {});
+  // const sourceFile = program.getSourceFile(fileName);
+
   fileName = forceTsExtension(posixPath(fileName));
 
-  const virtualFS = createVirtualFS([{ fileName, text }]);
+  const virtualFS = createVirtualFS([...libFiles, { fileName, text }]);
   const compilerHost = createCompilerHost(virtualFS);
-  const program = createProgram([fileName], options ?? {}, compilerHost);
+  const program = createProgram(
+    [...libFileNames, fileName],
+    options ?? {},
+    compilerHost
+  );
+  const sourceFile = virtualFS.get(fileName);
+
+  // --------
+
+  const files = program.getSourceFiles();
+  const compilerOptions = program.getCompilerOptions();
+  const diagnostics = program.getGlobalDiagnostics();
+  console.log({ program, files, compilerOptions, diagnostics });
+
+  // --------
+
   const checker = program.getTypeChecker();
 
-  const diagnostics = program.getGlobalDiagnostics();
-  console.log({ diagnostics });
-
-  return { program, checker, sourceFile: virtualFS.get(fileName)! };
+  return { program, checker, sourceFile: sourceFile! };
 }
 
 function getNearestInterfaceFromPosition(
