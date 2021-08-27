8a1382088cba2344a7cf7b0098f15758b4cd9066
skarab42 <skarab@bluewin.ch>
2021-08-27 14:26:02 +0200

    remove useless code

--- src/extension.ts
+++ src/extension.ts
@@ -5,15 +5,9 @@ import * as commands from "./commands";
 export function activate(context: vscode.ExtensionContext) {
   const { subscriptions } = context;
 
-  console.log("activated");
-
   subscriptions.push(
     ...Object.entries(commands).map(([name, command]) =>
       vscode.commands.registerCommand(`${pkg.name}.${name}`, command, context)
     )
   );
-}
-
-export function deactivate() {
-  console.log("deactivated");
 }
